91addef2a29f78e230e62dcbfd2d9082b4b35583
ErikaMon <erika.tatiana.monroy@gmail.com>
2015-09-13 13:25:51 -0500

    Proyecto

---
+++ AlineamientoSec/src/AlinearSec/AlineaSecuencia.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AlinearSec;
+
+/**
+ *
+ * @author JuanC
+ */
+public class AlineaSecuencia {
+
+    public AlineaSecuencia() {
+        
+        int matrizS[][] = new int[4][4];
+        // Inicializa matriz S
+        matrizS[0][0] = 10;
+        matrizS[0][1] = -1;
+        matrizS[0][2] = -3;
+        matrizS[0][3] = -4;
+        matrizS[1][0] = -1;
+        matrizS[1][1] = 7;
+        matrizS[1][2] = -5;
+        matrizS[1][3] = -3;
+        matrizS[2][0] = -3;
+        matrizS[2][1] = -5;
+        matrizS[2][2] = 9;
+        matrizS[2][3] = 0;
+        matrizS[3][0] = -4;
+        matrizS[3][1] = -3;
+        matrizS[3][2] = 0;
+        matrizS[3][3] = 8;
+    }
+
+}
